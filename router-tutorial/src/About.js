a39a1c83ae77e7e480f79ee04a638e95509d9422
worldi <jongha2788@u.sogang.ac.kr>
2021-12-05 04:05:49 +0900

    router version update

---
+++ router-tutorial/src/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+const About = () => {
+  return (
+    <div>
+      <h1>소개</h1>
+      <p>프로젝트 설명 : 예제 프로젝트 </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ router-tutorial/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Switch, Link } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
+
+const App = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profiles">프로필</Link>
+        </li>
+        <li>
+          <Link to="/history">History 예제</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />} exact={true} />
+        <Route path="/about" element={<About />} />
+        <Route path="/info" element={<About />}></Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
